12a75f9240456aa5ddc36dd840cdde600d989426
Adan <adanatif60@gmail.com>
2024-01-06 16:53:15 +0500

    mixed padding

--- app/(components)/Dashboard.tsx
+++ app/(components)/Dashboard.tsx
@@ -10,7 +10,7 @@ const Dashboard = () => {
 
   return (
     <>
-      <div className="grid grid-cols-1 lg:grid-cols-2 lg:h-screen h-auto relative">
+      <div className="grid grid-cols-1 lg:grid-cols-2 lg:h-screen h-auto relative px-4">
         <div className="flex flex-col  lg:ml-20 md:ml-8 sm:ml-6  lg:mt-8 md:mt-6 sm:t-2 mt-[6px] space-y-4 h-full w-full relative">
           <h1 className="text-2xl text-[#111029]  dark:text-exact-white md:text-[42px] md:leading-[52px] xl:text-[56px] xl:leading-[72px]  tracking-[-0.8px] font-semibold max-w-[500px] ">
             Welcome {!auth?.user?.username && `to`}
